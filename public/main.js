6376daf3619047b5be5ab5a895934a1c7a3f4ada
Liz Tiller <elizabeth.tiller@gmail.com>
2020-02-08 14:06:26 -0500

    working

--- public/main.js
+++ public/main.js
@@ -1,22 +1,58 @@
 const $ = (s) => document.querySelector(s)
 const $$ = s => document.querySelectorAll(s)
 
+let playerEngagementScore = 0
+let computerEngagementScore = 0
+
+let playerTextScore = $('.scores .player')
+
+let playerBoutScore = 0
+let computerBoutScore = 0
+
+const updatePlayerScore = () => {
+  playerEngagementScore += 1
+  $('.scores .player').textContent = playerEngagementScore
+  if (playerEngagementScore === 2) {
+    playerBoutScore += 1
+    $('.bout .player').textContent = playerBoutScore
+    playerEngagementScore = 0
+    $('.scores .player').textContent = playerEngagementScore
+    computerEngagementScore = 0
+    $('.scores .computer').textContent = computerEngagementScore
+
+    if (playerBoutScore === 2) {
+      gameOver(true)
+    }
+  }
+}
+
+const updateComputerScore = () => {
+  computerEngagementScore += 1
+  $('.scores .computer').textContent = computerEngagementScore
+  if (computerEngagementScore === 2) {
+    computerBoutScore += 1
+    $('.bout .computer').textContent = computerBoutScore
+    playerEngagementScore = 0
+    $('.scores .player').textContent = playerEngagementScore
+    computerEngagementScore = 0
+    $('.scores .computer').textContent = computerEngagementScore
+
+    if (computerBoutScore === 2) {
+      gameOver(false)
+    }
+  }
+}
+
 const handleButtonClick = (event) => {
   const player = event.target.className
   const computer = getComputerMove()
-  console.log(player, computer)
   $('figure.player img').src = `https://tiy-tpa-fee.github.io/roshambo/starter-kit/images/${player}.svg`
   $('figure.computer img').src = `https://tiy-tpa-fee.github.io/roshambo/starter-kit/images/${computer}.svg`
 
-  // HINT: Check for win, lose or draw, then call `gameOver()` eventually.
-  // playerDidWin === true
-  // if player === rock && computer === scissors
-  // if player === scissors && computer === paper
-  // if player === paper && computer === rock
   if ((player === 'rock' && computer === 'scissors') || (player === 'scissors' && computer === 'paper') || (player === 'paper' && computer === 'rock')) {
-    gameOver(true)
+    updatePlayerScore()
   } else if ((computer === 'rock' && player === 'scissors') || (computer === 'scissors' && player === 'paper') || (computer === 'paper' && player === 'rock')) {
-    gameOver(false)
+    updateComputerScore()
   } else {
     console.log("it's a tie!")
   }
@@ -24,14 +60,9 @@ const handleButtonClick = (event) => {
 
 const getComputerMove = () => {
   const moves = ['rock', 'paper', 'scissors']
-  // const randomNumber = Math.random() * moves.length
-  // const randomIndex = Math.floor(randomNumber)
-  // console.log(moves[randomIndex])
-  // return moves[randomIndex]
   return moves[Math.floor(Math.random() * moves.length)]
 }
 
-// HINT: Try calling `gameOver(true)` in the console.
 const gameOver = (didPlayerWin) => {
   if (didPlayerWin) {
     $('.dialog h3').textContent = 'You won!'
@@ -43,17 +74,26 @@ const gameOver = (didPlayerWin) => {
 
 const resetGame = () => {
   // TODO: Probably need to do more to reset the game here...
+  playerEngagementScore = 0
+  computerEngagementScore = 0
+  playerBoutScore = 0
+  computerBoutScore = 0
+  $('.scores .computer').textContent = computerEngagementScore
+  $('.scores .player').textContent = playerEngagementScore
+  $('.bout .player').textContent = playerBoutScore
+  $('.bout .computer').textContent = computerBoutScore
   $('figure.player img').src = 'https://tiy-tpa-fee.github.io/roshambo/starter-kit/images/unknown.svg'
   $('figure.computer img').src = 'https://tiy-tpa-fee.github.io/roshambo/starter-kit/images/unknown.svg'
   $('body').className = ''
 }
 
 const main = () => {
-  console.log('started')
   const buttons = $$('.player-input button')
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', handleButtonClick)
   }
+  playerTextScore.textContent = playerEngagementScore
+
   $('.dialog button').addEventListener('click', resetGame)
 }
 
